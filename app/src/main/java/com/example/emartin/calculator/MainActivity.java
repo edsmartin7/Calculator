a0ed08dbe0b5e830a736d310ba95e874ea94f350
Edward Martin <edward.martin@viacommix.com>
2017-11-13 18:48:02 -0500

    constraint layout attempt

--- app/src/main/java/com/example/emartin/calculator/MainActivity.java
+++ app/src/main/java/com/example/emartin/calculator/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        
+
         subtraction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
